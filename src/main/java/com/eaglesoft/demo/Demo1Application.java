b10345d5f59c35d55ac2946a97b854a3faf931be
Amit <amitdoiphode11@gmail.com>
2022-05-02 22:06:30 +0530

    -- Implement the springdoc-openapi

--- src/main/java/com/eaglesoft/demo/Demo1Application.java
+++ src/main/java/com/eaglesoft/demo/Demo1Application.java
@@ -1,9 +1,11 @@
 package com.eaglesoft.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class Demo1Application {
 
     public static void main(String[] args) {
